95e6a29386163e515e686cd8ec98e04750d83fba
ITzTravelInTime <pietrocaruso97@yahoo.it>
2022-06-09 06:36:43 +0200

    Version 0.0.16 - Added compliance to the `Copying` protocol to the `Dictionary` type.

---
+++ Sources/SwiftPackagesBase/Dictionary.swift
@@ -0,0 +1,25 @@
+/*
+ A Swift Library containing useful utilities code for Any Swift project.
+ Copyright (C) 2022 Pietro Caruso
+
+ This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+import Foundation
+
+extension Dictionary: Copying{
+    ///Creates a copy of the current instance
+    public func copy() -> Self {
+        var ret: Self = [:]
+        
+        for i in self{
+            ret[i.key] = i.value
+        }
+        
+        return ret
+    }
+}
